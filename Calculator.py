369bdbdb5f5da3e539400a056dca6d0db007788a
NxtPerfect <alakaxan@protonmail.com>
2022-07-04 18:57:11 +0200

    Fixed switch issue, match wouldn't work for some reason even tho i'm on python 3.10 and i wrote syntax correctly it threw an invalid syntax error. Changed that to if-elif function, added correct operation printing for sin cos and tan, added option to enter new numbers instead of having to quit of the app fully

--- Calculator.py
+++ Calculator.py
@@ -2,10 +2,6 @@ import os
 import math
 
 
-OPTIONS = {add(x, y), subtract(x, y), multiply(x, y), divide(x, y), power(
-    x, y), root(x, y), sinus(x, y), cosinus(x, y), tangent(x, y)}
-
-
 def clearConsole():
     command = 'clear'
     if os.name in ('nt', 'dos'):  # If windows use cls
@@ -13,12 +9,12 @@ def clearConsole():
     os.system(command)
 
 
-def add(x, y):
-    return (x + y)
+def addition(x, y):
+    return x + y
 
 
 def subtract(x, y):
-    return (x - y)
+    return x - y
 
 
 def multiply(x, y):
@@ -34,7 +30,7 @@ def power(x, y):
 
 
 def root(x, y):
-    if y is 2:
+    if y == 2:
         return math.sqrt(x)  # or return x**(1/2)
     return math.pow(x, (1/y))  # or return x**(1/y)
 
@@ -49,6 +45,29 @@ def cosinus(x):
 
 def tangent(x):
     return math.tan(x)
+
+
+def switch(option, x, y):
+
+    if option == '+':
+        return (addition(x, y))
+    elif option == '-':
+        return (subtract(x, y))
+    elif option == '*':
+        return (multiply(x, y))
+    elif option == '/':
+        return (divide(x, y))
+    elif option == "pow":
+        return (power(x, y))
+    elif option == "root":
+        return (root(x, y))
+    elif option == 'sin':
+        return (sinus(x))
+    elif option == 'cos':
+        return (cosinus(x))
+    elif option == 'tan':
+        return (tangent(x))
+    return ("That's not a valid option")
 
 
 if __name__ == "__main__":
@@ -66,21 +85,23 @@ if __name__ == "__main__":
 
         while True:
             try:
-                choice = input(
-                    "What would you like to do?\nAdd two numbers(1)\nSubtract(2)\nMultiply(3)\nDivide(4)\nPower(5)\nRoot(6)\nSin(7)\nCos(8)\nTan(9)\nExit(x)\n\n")
+                choice = str.lower(input(
+                    "What would you like to do?\nAddition(+)\nSubtract(-)\nMultiply(*)\nDivide(/)\nPower(pow)\nRoot(root)\nSinus(sin)\nCosinus(cos)\nTangent(tan)\nPick new numbers(p)\nExit(x)\n\n"))
             except choice == NULL or choice == "\n" or choice == " ":
                 print("That's not a valid option.")
                 continue
 
-            if (choice != 'x'):
-                print(str(x) + " " + choice + " " + str(y) + " = ")
-            else:
-                break
-
-            if choice == 'x':
+            if (choice == 'x'):
                 exit
 
-            print(OPTIONS[choice])
+            if (choice == 'p'):
+                clearConsole()
+                break
+            if (choice != "sin" or choice != "tan"):
+                print(str(x) + " " + choice + " " + str(y) + " = ")
+            print(str(x) + " " + choice + " = ")
+
+            print(switch(choice, x, y))
 
             input()
 
