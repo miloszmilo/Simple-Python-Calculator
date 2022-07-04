6dda594a7953b277e97eaa970b7af7e0e11d0d1d
NxtPerfect <alakaxan@protonmail.com>
2022-07-04 17:49:11 +0200

    Added power, root, sinus, cosinus and tangent functions. Made the "switch" code not use 7 else if loops, instead I added one constant dictionary with all the options, which then uses choice input as a number to dictate which option was chosen

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,9 @@
 import os
+import math
+
+
+OPTIONS = {add(x, y), subtract(x, y), multiply(x, y), divide(x, y), power(
+    x, y), root(x, y), sinus(x, y), cosinus(x, y), tangent(x, y)}
 
 
 def clearConsole():
@@ -16,12 +21,34 @@ def subtract(x, y):
     return (x - y)
 
 
+def multiply(x, y):
+    return float(x * y)
+
+
 def divide(x, y):
     return float(x / y)
 
 
-def multiply(x, y):
-    return float(x * y)
+def power(x, y):
+    return math.pow(x, y)
+
+
+def root(x, y):
+    if y is 2:
+        return math.sqrt(x)  # or return x**(1/2)
+    return math.pow(x, (1/y))  # or return x**(1/y)
+
+
+def sinus(x):
+    return math.sin(x)
+
+
+def cosinus(x):
+    return math.sin(x)
+
+
+def tangent(x):
+    return math.tan(x)
 
 
 if __name__ == "__main__":
@@ -40,7 +67,7 @@ if __name__ == "__main__":
         while True:
             try:
                 choice = input(
-                    "What would you like to do?\n1. Add two numbers(+)\n2. Subtract(-)\n3. Divide(/)\n4. Multiply(*)\n5. Exit(x)\n\n")
+                    "What would you like to do?\nAdd two numbers(1)\nSubtract(2)\nMultiply(3)\nDivide(4)\nPower(5)\nRoot(6)\nSin(7)\nCos(8)\nTan(9)\nExit(x)\n\n")
             except choice == NULL or choice == "\n" or choice == " ":
                 print("That's not a valid option.")
                 continue
@@ -50,31 +77,11 @@ if __name__ == "__main__":
             else:
                 break
 
-            if choice == '+':
-                print(add(x, y))
-            elif choice == '-':
-                print(subtract(x, y))
-            elif choice == '/':
-                print(divide(x, y))
-            elif choice == '*':
-                print(multiply(x, y))
-            elif choice == 'x':
-                print(exit())
-            else:
-                print("Not a valid option.")
-                continue
+            if choice == 'x':
+                exit
+
+            print(OPTIONS[choice])
 
             input()
 
             clearConsole()
-        # match choice:
-        #     case '1':
-        #         print(add(x, y))
-        #     case '2':
-        #         print(subtract(x, y))
-        #     case '3':
-        #         print(divide(x, y))
-        #     case '4':
-        #         print(multiply(x, y))
-        #     case '5':
-        #         print(exit())
